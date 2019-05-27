d13bd8124d9467d2d1ade452804a6e2c69588d38
tomaskrajnik <tomas.krajni@gmail.com>
2019-05-28 00:04:58 +0200

    News

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var burger = document.querySelector('.bar__burger');
+var bar = document.querySelector('.bar');
+
+burger.addEventListener('click', function (){
+  burger.classList.toggle('bar__burger-open');
+bar.classList.toggle('bar-open');
+
+
+
+
+
+});
